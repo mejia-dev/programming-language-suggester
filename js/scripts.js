8adebfbc19f8d23a72104a702fb91d6956550423
mejia-dev <aaron.mejia8484@gmail.com>
2023-10-20 15:15:25 -0700

    Add a field for the user's name and return it in the resultText

--- js/scripts.js
+++ js/scripts.js
@@ -2,37 +2,38 @@ let darkMode = false;
 
 function getAnswers() {
   event.preventDefault();
+  let name = document.getElementById("userName").value;
   let answerA = parseInt(document.getElementById("questionA").value);
   let answerB = document.getElementById("questionB").value;
   let answerC = (document.getElementById("questionC").value).length;
   let answerD = document.querySelector("input[name=questionD]:checked").value;
   let answerE = document.getElementById("questionE").value;
-  showResults(answerA, answerB, answerC, answerE);
+  showResults(name, answerA, answerB, answerC, answerE);
   showFrogMessage(answerD);
 }
 
-function showResults(in1, in2, in3, in4) {
+function showResults(in0, in1, in2, in3, in4) {
   if (in1 >= 3) {
     if (in2.includes("e") && (in3 > 6)) {
       document.getElementById("resultHeader").innerHTML = "You got: <span id=resultHeaderTitle style='color:" + in4 + "'>Python!</span>";
-      document.getElementById("resultText").innerHTML = "Click <a href='https://google.com/search?q=learn+how+to+code+in+python' target='blank'>here</a> to search for resources online.";
+      document.getElementById("resultText").innerHTML = "Click <a href='https://google.com/search?q=learn+how+to+code+in+python' target='blank'>here</a> to search for resources online, "+ in0 + ".";
     } else if (in2.includes("e") && (in3 <= 6)) {
       document.getElementById("resultHeader").innerHTML = "You got: <span id=resultHeaderTitle style='color:" + in4 + "'>Ruby!</span>";
-      document.getElementById("resultText").innerHTML = "Click <a href='https://google.com/search?q=learn+how+to+code+in+ruby' target='blank'>here</a> to search for resources online.";
+      document.getElementById("resultText").innerHTML = "Click <a href='https://google.com/search?q=learn+how+to+code+in+ruby' target='blank'>here</a> to search for resources online, "+ in0 + ".";
     } else {
       document.getElementById("resultHeader").innerHTML = "You got: <span id=resultHeaderTitle style='color:" + in4 + "'>Ruby!</span>";
-      document.getElementById("resultText").innerHTML = "Click <a href='https://google.com/search?q=learn+how+to+code+in+ruby' target='blank'>here</a> to search for resources online.";
+      document.getElementById("resultText").innerHTML = "Click <a href='https://google.com/search?q=learn+how+to+code+in+ruby' target='blank'>here</a> to search for resources online, "+ in0 + ".";
     }
   } else {
     if (in2.includes("e") && (in3 > 4)) {
       document.getElementById("resultHeader").innerHTML = "You got: <span id=resultHeaderTitle style='color:" + in4 + "'>Rust!</span>";
-      document.getElementById("resultText").innerHTML = "Click <a href='https://google.com/search?q=learn+how+to+code+in+rust' target='blank'>here</a> to search for resources online.";
+      document.getElementById("resultText").innerHTML = "Click <a href='https://google.com/search?q=learn+how+to+code+in+rust' target='blank'>here</a> to search for resources online, "+ in0 + ".";
     } else if (in2.includes("e") && (in3 <= 4)) {
       document.getElementById("resultHeader").innerHTML = "You got: <span id=resultHeaderTitle style='color:" + in4 + "'>Ruby!</span>";
-      document.getElementById("resultText").innerHTML = "Click <a href='https://google.com/search?q=learn+how+to+code+in+ruby' target='blank'>here</a> to search for resources online.";
+      document.getElementById("resultText").innerHTML = "Click <a href='https://google.com/search?q=learn+how+to+code+in+ruby' target='blank'>here</a> to search for resources online, "+ in0 + ".";
     } else {
       document.getElementById("resultHeader").innerHTML = "You got: <span id=resultHeaderTitle style='color:" + in4 + "'>Ruby!</span>";
-      document.getElementById("resultText").innerHTML = "Click <a href='https://google.com/search?q=learn+how+to+code+in+ruby' target='blank'>here</a> to search for resources online.";
+      document.getElementById("resultText").innerHTML = "Click <a href='https://google.com/search?q=learn+how+to+code+in+ruby' target='blank'>here</a> to search for resources online, "+ in0 + ".";
     }
   }
 }
