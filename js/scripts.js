fd831a1367974db069ad49f707a9004f29db9332
mejia-dev <aaron.mejia8484@gmail.com>
2023-10-20 10:34:51 -0700

    Determine structure for how results should be generated. Add comment under showResults to demonstrate intended result. Add all if statements in accordance with the intended result (currently does not give output)

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,41 @@ function getAnswers() {
 }
 
 function showResults(in1,in2,in3,in4,in5) {
+  // in1 Greater than or equal to 3, you're destined for Ruby or Python
+    // in2 include the letter "e" AND in3 is greater than 6, you're destined for Python
+    // in2 include the letter "e" AND in3 is less than or equal to 6, you're destined for Ruby
+    // in2 not include letter "e", you're destined for Ruby
+
+  // in1 less than 3, you're destined for Ruby or CSharp
+    // in2 include the letter "e" AND in3 is greater than 4, you're destined for CSharp
+    // in2 include the letter "e" AND in3 is less than or equal to 4, you're destined for Ruby
+    // in2 not include letter "e", you're destined for Ruby
+
+  // in4 and in5 are just extra data for the time being and will be an extra feature in the page later
   
+  if (in1 >= 3) {
+    if (in2.includes("e") && (in3 > 6)) { 
+      //Python!
+    } else if (in2.includes("e") && (in3 <= 6)) {
+      //Ruby!
+    } else {
+      //Ruby!
+    }
+  } else {
+    if (in2.includes("e") && (in3 > 4)) { 
+      //CSharp!
+    } else if (in2.includes("e") && (in3 <= 4)) {
+      //Ruby!
+    } else {
+      //Ruby!
+    }
+  }
 }
 
 // UI
 window.addEventListener("load", function() {
   this.document.getElementById("questionInput").addEventListener("submit", getAnswers)
 })
+
+
+
