accb459f1b0b95900c9d2397b85fa2b20572be9e
mejia-dev <aaron.mejia8484@gmail.com>
2023-10-20 10:12:12 -0700

    Modify getAnswers function to establish variables for answers B and D.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 function getAnswers() {
+  event.preventDefault();
   let answerA = parseInt(document.getElementById("questionA").value);
-  // let answerB = 
+  let answerB = document.getElementById("questionB").value;
   let answerC = (document.getElementById("questionC").value).length;
-  // let answerD = 
-  let answerE = document.getElementById("questionA").value;
+  let answerD = document.querySelector("input[name=questionD]:checked").value;
+  let answerE = document.getElementById("questionE").value;
   showResults(answerA,answerB,answerC,answerD,answerE);
 }
 
 function showResults(in1,in2,in3,in4,in5) {
-
+  
 }
 
 // UI
 window.addEventListener("load", function() {
-  event.preventDefault();
   this.document.getElementById("questionInput").addEventListener("submit", getAnswers)
 })
